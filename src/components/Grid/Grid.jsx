e7cddae344248488b12cf6497ef97440f22ead98
Vlad Solomon <vladsolomon720@gmail.com>
2024-07-16 20:31:00 +0300

    Implement Tippy for Grid children

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,12 +2,16 @@ import "./Grid.scss";
 import data from "./data.json";
 import getColor from "../../utils/getColor";
 import getMean from "../../utils/getMean";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 export function Grid() {
 	return (
 		<div className="grid">
 			{data.map(({ id, year, temperatures }) => (
-				<div key={id} className="mock-square" style={{ backgroundColor: getColor(getMean(temperatures)) }}></div>
+				<Tippy key={id} content={year} placement="right" theme="default" animation={false}>
+					<div className="mock-square" style={{ backgroundColor: getColor(getMean(temperatures)) }}></div>
+				</Tippy>
 			))}
 		</div>
 	);
