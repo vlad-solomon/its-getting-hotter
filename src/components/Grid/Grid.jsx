201d4560c98369a5b5ddf7c92e4937592d95f188
Vlad Solomon <vladsolomon720@gmail.com>
2024-07-16 18:04:57 +0300

    Create getColor function

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,13 +1,12 @@
 import "./Grid.scss";
 import data from "./data.json";
+import getColor from "../../utils/getColor";
 
 export function Grid() {
 	return (
 		<div className="grid">
 			{data.map(({ id, year, temperatures }) => (
-				<div key={id} className="mock-square">
-					{year}
-				</div>
+				<div key={id} className="mock-square" style={{ backgroundColor: getColor(temperatures.reduce((acc, curr) => acc + curr, 0) / temperatures.length) }}></div>
 			))}
 		</div>
 	);
---
+++ src/utils/getColor.js
@@ -0,0 +1,21 @@
+const colors = {
+	13.5: "00876c",
+	13.6875: "50a26f",
+	13.875: "88bb72",
+	14.0625: "c1d379",
+	14.25: "fde987",
+	14.4375: "fac067",
+	14.625: "f49654",
+	14.8125: "e86b4e",
+	15: "d43d51",
+};
+
+export default function (value) {
+	let nearestKey;
+	for (let key in colors) {
+		if (key <= value) {
+			nearestKey = key;
+		}
+	}
+	return `#${colors[nearestKey]}`;
+}
